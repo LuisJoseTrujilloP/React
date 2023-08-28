94eb4c36484e70fa62585fa3b929155eca60f8ca
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-08-28 14:45:37 -0400

    Working with components, finished class 1

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+      <TodoItem/>
+
     </div>
+  );
+}
+
+
+// CREATING, FIRST COMPONENT 
+function TodoItem(){
+    return (
+      <div>
+      <li>
+        <span>V</span>
+        <p>Llorar con la llorona</p>
+        <span>X</span>
+      </li>      
+      </div>
   );
 }
 
