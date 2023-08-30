19d955a06682516c1685dcb8b8d9049ce2fe40ba
LuisJoseTrujilloP <luisjosetrujillodeveloper@gmail.com>
2023-08-30 00:17:55 -0400

    Working with the state inside TodoSearch

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,6 +2,7 @@ import './CreateTodoButton.css'
 import React from 'react'
 
 function CreateTodoButton() {
+  
   return (
     <button className='CreateTodoButton' onClick={(event) => { 
       console.log( event.target )
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,19 @@
+import React from 'react';
 import './TodoSearch.css'
 
 function TodoSearch(){
+    
+    const [state, setState] = React.useState('');
+     // To work with the state we cant just change it as we would with any variable, we need to use a function to work with it
+    // Changer function
+
     return(
             <input 
             className='TodoSearch' 
             placeholder="Search Todo"
+            value={state}
             onChange={(event) => {
-                console.log(event.target, event.target.value)
+                setState(event.target.value)
             }}
             >
 
